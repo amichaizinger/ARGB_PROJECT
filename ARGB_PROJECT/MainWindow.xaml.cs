1bcdc17cd6dba48129421e48d5e35d922e1f87a7
amichaizinger <zinger.amichai@gmail.com>
2025-02-06 21:57:22 +0200

    the finished project

--- ARGB_PROJECT/MainWindow.xaml.cs
+++ ARGB_PROJECT/MainWindow.xaml.cs
@@ -41,10 +41,11 @@ namespace RGB
             if (open.ShowDialog() == true)
             {             
                 string ImageSource = open.FileName;
-                StatusText.Text = "Perfect we have a picture lets proceed";
+                StatusText.Text = "press the button (it might take a few seconds)";
                 bitmap = new Bitmap(ImageSource);
+                image.BeginInit();
                 image.UriSource = new Uri(ImageSource);
-
+                image.EndInit();
                 UploadedImage.Source = image;
                 UploadedImage.Visibility = Visibility.Visible;
                 convert.Visibility = Visibility.Visible;
@@ -52,35 +53,60 @@ namespace RGB
             }
         }
 
-        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        private async void ConvertButton_Click(object sender, RoutedEventArgs e)
         {
+            UploadedImage.Visibility = Visibility.Collapsed;
+            convert.Visibility = Visibility.Collapsed;
             ColorPics();
-            }
+            StatusText.Text = "Conversion completed!";
+            UploadedImage.Visibility = Visibility.Collapsed;
+            original.Source = image;
+            chooseImage.Visibility = Visibility.Visible;
+        }
+
         public void ColorPics()
         {
-            int height = (int)(image.Height);
-            int width = (int)(image.Width);
-            Bitmap RedBitmap= new Bitmap( 200, 200);
-            Bitmap BlueBitmap= new Bitmap(width, height);
-            Bitmap GreenBitmap= new Bitmap(width, height);
+            // Your heavy task (e.g., image processing)
+            int height = bitmap.Height;  // Get height from System.Drawing.Bitmap
+            int width = bitmap.Width;    // Get width from System.Drawing.Bitmap
 
-            for (int i = 0; i < image.Width; i++)
+            Bitmap RedBitmap = new Bitmap(width, height);
+            Bitmap GreenBitmap = new Bitmap(width, height);
+            Bitmap BlueBitmap = new Bitmap(width, height);
+
+            for (int i = 0; i < width; i++)
             {
-                for (int j = 0; j < image.Height; j++)
+                for (int j = 0; j < height; j++)
                 {
                     System.Drawing.Color pixelColor = bitmap.GetPixel(i, j);
-                    int r = 0, g = 0, b = 0;
-                    r = pixelColor.R;
-                    g = pixelColor.G;
-                    b = pixelColor.B;
-                    GreenBitmap.SetPixel(i, j, System.Drawing.Color.FromArgb(0, g, 0));
-                    BitmapImage GreenImage = ConvertBitmapToBitmapImage(GreenBitmap);
-                    green.Source=GreenImage;
-                    Pictures.Visibility = Visibility.Visible;
-                }  
+                    int r = pixelColor.R;
+                    int g = pixelColor.G;
+                    int b = pixelColor.B;
+
+                    // Set pixels for each color channel
+                    RedBitmap.SetPixel(i, j, System.Drawing.Color.FromArgb(r, 0, 0));  // Red channel
+                    GreenBitmap.SetPixel(i, j, System.Drawing.Color.FromArgb(0, g, 0));  // Green channel
+                    BlueBitmap.SetPixel(i, j, System.Drawing.Color.FromArgb(0, 0, b));  // Blue channel
+                }
             }
 
+            // Convert Bitmaps to BitmapImages for display
+            BitmapImage RedImage = ConvertBitmapToBitmapImage(RedBitmap);
+            BitmapImage GreenImage = ConvertBitmapToBitmapImage(GreenBitmap);
+            BitmapImage BlueImage = ConvertBitmapToBitmapImage(BlueBitmap);
+
+
+            Dispatcher.Invoke(() =>
+            {
+                red.Source = RedImage;
+                green.Source = GreenImage;
+                blue.Source = BlueImage;
+                Pictures.Visibility = Visibility.Visible;
+            });
         }
+
+
+
 
         static BitmapImage ConvertBitmapToBitmapImage(Bitmap bitmap)
         {
